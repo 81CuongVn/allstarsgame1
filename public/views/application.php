3cafd9f0731b7101fcf6550821406669da94095d
Felipe Medeiros <medeiros.dev@gmail.com>
2021-03-08 06:51:32 -0300

    Atualização 08/03/20221

--- public/views/application.php
+++ public/views/application.php
@@ -337,7 +337,7 @@ if ($_SESSION['user_id']) {
 						<div>
 							<script id="_waulxb">var _wau = _wau || []; _wau.push(["classic", "zp4gq851mt0f", "lxb"]);
 							(function() {var s=document.createElement("script"); s.async=true;
-							s.src="http://widgets.amung.us/classic.js";
+							s.src="//widgets.amung.us/classic.js";
 							document.getElementsByTagName("head")[0].appendChild(s);
 							})();</script>
 						</div>
