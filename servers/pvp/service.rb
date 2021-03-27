0f76904c8c2ab39f2ce7799b7191f91310149ef5
Felipe Medeiros <medeiros.dev@gmail.com>
2021-03-26 22:40:55 -0300

    Att PvP Queue

--- servers/pvp/service.rb
+++ servers/pvp/service.rb
@@ -4,12 +4,10 @@ require 'daemons'
 
 module Daemons
 	class Application
-		def logfile;		'./logs/queue_log.log'; end
-		def output_logfile;	'./logs/queue_output.log'; end
+		def logfile;		'/var/www/allstarsgame/servers/pvp/logs/queue_log.log'; end
+		def output_logfile;	'/var/www/allstarsgame/servers/pvp/logs/queue_output.log'; end
 		def backtrace;		true; end
 	end
 end
 
-Daemons.run './queue.prod.rb', dir: './logs', dir_mode: :normal, ontop: false, log_output: true
-# Dir.mkdir('/var/www/allstarsgame/servers/pvp', 0777) rescue nil
-# Daemons.run '/var/www/allstarsgame/servers/pvp/queue.prod.rb', dir: '/var/www/allstarsgame/servers/pvp', dir_mode: :normal, ontop: false, log_output: true
+Daemons.run '/var/www/allstarsgame/servers/pvp/queue.prod.rb', dir: '/var/www/allstarsgame/servers/pvp', dir_mode: :normal, ontop: false, log_output: true
