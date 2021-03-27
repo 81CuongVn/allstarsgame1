9800fd006ae5984bcd4c7034ab171c6256960878
Felipe Medeiros <medeiros.dev@gmail.com>
2021-03-26 22:32:12 -0300

    Att PvP Queue

--- servers/pvp/service.rb
+++ servers/pvp/service.rb
@@ -2,23 +2,15 @@
 
 require 'daemons'
 
-# module Daemons
-# 	class Application
-# 		def logfile;		'/var/www/allstarsgame/servers/pvp/logs/pvp_queue.log'; end
-# 		def output_logfile;	'/var/www/allstarsgame/servers/pvp/logs/pvp_queue2.log'; end
-# 	end
-# end
+module Daemons
+	class Application
+		def logfile;		'/var/www/allstarsgame/servers/pvp/logs/queue_log.log'; end
+		def output_logfile;	'/var/www/allstarsgame/servers/pvp/logs/queue_output.log'; end
+		def backtrace;		true; end
+	end
+end
 
-options = {
-  :log_output			=> true,
-  :backtrace			=> true,
-  :output_logfilename	=> "queue_output.txt",
-  :logfilename			=> "queue_log.log",
-  :dir_mode				=> :normal,
-  :ontop				=> false,
-  :dir					=> File.join(File.dirname(__FILE__)
-}
-Daemons.run(File.join(File.dirname(__FILE__), 'queue.prod.rb'), options)
-
+queue_file	= File.join(File.dirname(__FILE__), 'queue.prod.rb');
+Daemons.run queue_file, dir: '/var/www/allstarsgame/servers/pvp', dir_mode: :normal, ontop: false, log_output: true
 # Dir.mkdir('/var/www/allstarsgame/servers/pvp', 0777) rescue nil
 # Daemons.run '/var/www/allstarsgame/servers/pvp/queue.prod.rb', dir: '/var/www/allstarsgame/servers/pvp', dir_mode: :normal, ontop: false, log_output: true
