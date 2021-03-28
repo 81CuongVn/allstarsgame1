ceae198d6b0d4628a92fda02d8878c9c1d3de5ec
Felipe Medeiros <medeiros.dev@gmail.com>
2021-03-28 03:41:48 -0300

    Atualização 28/03/2021

--- cronjob/_config.php
+++ cronjob/_config.php
@@ -10,7 +10,7 @@ define('BACKTRACE_DELETES',         TRUE);
 define('ROOT',						dirname(dirname(__FILE__)));
 define('RECORDSET_CACHE_OFF_FORCE', TRUE);
 
-$env = 'dev';
+$env = 'prod';
 if (array_key_exists('SERVER_ADDR', $_SERVER) && in_array($_SERVER['SERVER_ADDR'], [ '127.0.0.1' ])) {
 	$env = 'dev';
 }
