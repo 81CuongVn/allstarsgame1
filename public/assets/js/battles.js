0139d5dd89f679861a69a408221a12591bb2fe5b
Felipe Medeiros <medeiros.dev@gmail.com>
2021-03-29 14:00:29 -0300

    Atualização III 29/03/2021

--- public/assets/js/battles.js
+++ public/assets/js/battles.js
@@ -300,10 +300,10 @@
 					$('.activatables-player .modifier-turn-data').remove();
 					$('.activatables-player .technique-popover').css({opacity: 1});
 
-					//if ($('.activatables-enemy .modifier-turn-data').length && result.enemy.update_existent_locks) {
+					if ($('.activatables-enemy .modifier-turn-data').length && result.enemy.update_existent_locks) {
 						$('.activatables-enemy .modifier-turn-data').remove();
 						$('.activatables-enemy .technique-popover').css({opacity: 1});
-					//}
+					}
 
 					result.enemy.locks.forEach(function (item) {
 						if (item.type == 'speciality' || item.type == 'ability') {
