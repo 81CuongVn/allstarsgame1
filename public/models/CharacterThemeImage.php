3ab46519d054024e2532b1528b94cbc4ab552720
Felipe Medeiros <medeiros.dev@gmail.com>
2021-05-10 21:14:29 -0300

    Ultimate Fix

--- public/models/CharacterThemeImage.php
+++ public/models/CharacterThemeImage.php
@@ -15,16 +15,11 @@
 			if($path_only) {
 				return $path;
 			} else {
-				// if ($this->ultimate) {
-				// 	$timestamp	= filemtime(ROOT . '/assets/images/' . $path);
-
-				// 	return '<embed height="281" width="235" src="' . image_url($path) . '?_cache=' . $timestamp . '" quality="high" wmode="transparent" allowscriptaccess="always" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash"></embed>';
-				// } else {
-					return '<img src="' . image_url($path) . '" alt="' . $character->description()->name . '" />';
-				// }
+				$timestamp	= filemtime(ROOT . '/assets/images/' . $path);
+				return '<img src="' . image_url($path) . '?_cache=' . $timestamp . '" alt="' . $character->description()->name . '" />';
 			}
 		}
-		
+
 		function small_image($path_only = false) {
 			$theme		= $this->character_theme();
 			$character	= $theme->character();
