8a7f6f22d6eb13537d6c98744070d6ea61e7e554
Felipe Medeiros <medeiros.dev@gmail.com>
2021-05-30 20:15:29 -0300

    Att.

--- public/views/shared/_footer.php
+++ public/views/shared/_footer.php
@@ -2,7 +2,7 @@
     <div id="rodape-posicao">
         <div id="facebook">
             <div style="width: 385px; height: 70px;">
-                <div class="fb-page" data-href="https://www.facebook.com/<?=FB_PAGE_USER;?>" data-tabs="timeline" data-width="385" data-height="70" data-small-header="true" data-adapt-container-width="false" data-hide-cover="true" data-show-facepile="false"></div>
+                <!-- <div class="fb-page" data-href="https://www.facebook.com/<?=FB_PAGE_USER;?>" data-tabs="timeline" data-width="385" data-height="70" data-small-header="true" data-adapt-container-width="false" data-hide-cover="true" data-show-facepile="false"></div> -->
             </div>
         </div>
         <div id="texto-rodape">
@@ -11,18 +11,18 @@
             </p>
         </div>
         <div id="outros-jogos">
-			<div style="padding-top: 12px;">
-				<a href="https://sugoigame.com.br" target="_blank">
-					<img src="<?=image_url('logos/sugoi-logo.png')?>" alt="Sugoi Game" />
+			<!-- <div style="padding-top: 12px;">
+				<a href="https://bleachforce.com.br" target="_blank">
+					<img src="<?=image_url('logos/bg-logo.png')?>" alt="Bleach Force" />
 				</a>
 				<div class="clearfix"></div>
-			</div>
-			<div style="padding-top: 20px;">
+			</div> -->
+			<!-- <div style="padding-top: 20px;">
 				<a href="https://bleach.sugoigame.com.br" target="_blank">
 					<img src="<?=image_url('logos/bg-logo.png')?>" alt="Bleach Game" />
 				</a>
 				<div class="clearfix"></div>
-			</div>
+			</div> -->
 		</div>
     </div>
 </div>
