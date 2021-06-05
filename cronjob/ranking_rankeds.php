34c52c90c19029433e7bbcac2dae8c8716825698
Felipe Medeiros <medeiros.dev@gmail.com>
2021-06-05 07:48:59 -0300

    Att

--- cronjob/ranking_rankeds.php
+++ cronjob/ranking_rankeds.php
@@ -4,7 +4,7 @@ require '_config.php';
 Recordset::query('TRUNCATE TABLE ranking_rankeds');
 
 $factions	= Recordset::query('SELECT id FROM factions WHERE active = 1');
-$rankeds	= Recordset::query('SELECT league FROM rankeds WHERE finished = 0');
+$rankeds	= Recordset::query('SELECT * FROM rankeds WHERE finished = 0');
 foreach ($rankeds->result_array() as $ranked) {
 	foreach ($factions->result_array() as $faction) {
 		$players	= Recordset::query('
@@ -17,25 +17,25 @@ foreach ($rankeds->result_array() as $ranked) {
 				a.character_theme_id,
 				a.faction_id,
 				a.level,
-				d.sorting AS graduation_level,
+				e.points,
 				e.wins,
 				e.losses,
 				e.draws,
-				e.league,
-				e.rank
-
+				e.ranked_id,
+				e.ranked_tier_id
 			FROM
-				players a JOIN character_themes b ON b.id=a.character_theme_id
-				JOIN characters c ON c.id=a.character_id
-				JOIN graduations d ON d.id=a.graduation_id
-				JOIN player_rankeds e ON e.player_id=a.id
-
+				players a
+				JOIN character_themes b ON b.id = a.character_theme_id
+				JOIN characters c ON c.id = a.character_id
+				JOIN graduations d ON d.id = a.graduation_id
+				JOIN player_rankeds e ON e.player_id = a.id
 			WHERE
-				a.faction_id=' . $faction['id'].' AND e.league = '.$ranked['league'].' AND a.removed = 0 AND a.banned = 0
-				GROUP BY a.id
+				a.faction_id = ' . $faction['id'].' AND
+				e.ranked_id = ' . $ranked['id'] . ' AND
+				a.removed = 0 AND
+				a.banned = 0
 		');
 		foreach ($players->result_array() as $player) {
-			$points	= ($player['rank'] ==  0 ? 200000 : (100000 / $player['rank'])) + ( $player['wins'] * 50 );
 			Recordset::insert('ranking_rankeds', [
 				'player_id'				=> $player['id'],
 				'anime_id'				=> $player['anime_id'],
@@ -45,17 +45,17 @@ foreach ($rankeds->result_array() as $ranked) {
 				'faction_id'			=> $player['faction_id'],
 				'name'					=> $player['name'],
 				'level'					=> $player['level'],
-				'score'					=> $points,
-				'league_id'				=> $player['league'],
+				'score'					=> $player['points'],
+				'ranked_id'				=> $player['ranked_id'],
 				'losses'				=> $player['losses'],
 				'draws'					=> $player['draws'],
 				'wins'					=> $player['wins'],
-				'rank'					=> $player['rank']
+				'ranked_tier_id'		=> $player['ranked_tier_id']
 			]);
 		}
 
 		$position	= 1;
-		$players	= Recordset::query('SELECT id FROM ranking_rankeds WHERE league_id='.$ranked['league'].' AND faction_id=' . $faction['id'] . '  ORDER BY `score` DESC, `level` DESC');
+		$players	= Recordset::query('SELECT id FROM ranking_rankeds WHERE ranked_id = ' . $ranked['id'] . ' AND faction_id = ' . $faction['id'] . '  ORDER BY `score` DESC, `level` DESC');
 		foreach ($players->result_array() as $player) {
 			Recordset::update('ranking_rankeds', [
 				'position_faction'	=> $position++
@@ -63,9 +63,10 @@ foreach ($rankeds->result_array() as $ranked) {
 				'id'				=> $player['id']
 			]);
 		}
+	}
 
 	$position	= 1;
-	$players	= Recordset::query('SELECT id FROM ranking_rankeds WHERE league_id='.$ranked['league'].' ORDER BY `score` DESC, `level` DESC');
+	$players	= Recordset::query('SELECT id FROM ranking_rankeds WHERE ranked_id = ' . $ranked['id'] . ' ORDER BY `score` DESC, `level` DESC');
 	foreach($players->result_array() as $player) {
 		Recordset::update('ranking_rankeds', [
 			'position_general'	=> $position++
@@ -73,7 +74,6 @@ foreach ($rankeds->result_array() as $ranked) {
 			'id'				=> $player['id']
 		]);
 	}
-}
 }
 
 echo "[Ranking Ranked] Cron executada com sucesso!\n";
