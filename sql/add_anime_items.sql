90d87fe790ea3313a3811ba2a2ca8bf50269fbb9
Felipe Medeiros <medeiros.dev@gmail.com>
2021-06-11 20:53:40 -0300

    Add Files

---
+++ sql/add_anime_items.sql
@@ -0,0 +1,58 @@
+INSERT INTO item_descriptions (anime_id,item_id,item_attack_type_id,language_id,name,description,image) VALUES
+	 (41,1,5,1,'Shuriken','Arremessa uma shuriken no inimigo.','shuriken.png'),
+	 (41,3,0,1,'Reflexos Ninja','O ninja tem a capacidade de pensar rápido e executar ações de uma forma rápida e eficiente.','reflexos-ninja.png'),
+	 (41,4,4,1,'Kunai','Arremessa uma kunai no inimigo ou usa em combate corpo a corpo.','kunai.png'),
+	 (41,7,2,1,'Rasteira','Desfere uma rápida rasteira no inimigo.','rasteira.png'),
+	 (41,6,5,1,'Defender com a Shuriken','Arremessa shurikens para se defender de golpes a longa distância.','defender-shuriken.png'),
+	 (41,8,3,1,'Múltiplas Shurikens','Arremessa múltiplas shurikens em alta velocidade.','multiplas-shurikens.png'),
+	 (41,9,0,1,'Concentração de Chakra','Torna-se capaz de concentrar chakra em seus pés para andar sobre a água.','concentracao-chakra.png'),
+	 (41,11,1,1,'Henge no Jutsu','Um jutsu que copia uma pessoa, animal ou objeto, usado para distrair o inimigo.','henge-no-jutsu.png'),
+	 (41,14,2,1,'Joelhada','Desfere uma joelhada no estômago do inimigo.','joelhada.png'),
+	 (41,13,4,1,'Defender com a Kunai','Utiliza uma kunai para bloquear golpes.','defender-kunai.png');
+INSERT INTO item_descriptions (anime_id,item_id,item_attack_type_id,language_id,name,description,image) VALUES
+	 (41,15,3,1,'Palmas de Chakra','Concentra chakra nas mãos para golpear o inimigo.','chakura-no-mesu.png'),
+	 (41,16,0,1,'Determinação Ninja','Um ninja faz de tudo quando encontra-se em uma situação de risco.','determinacao-ninja.png'),
+	 (41,18,5,1,'Kunai Explosiva','Arremessa uma kunai com papel explosivo preso, explodindo ao entrar em contato com alguma coisa.','kunai-explosiva.png'),
+	 (41,21,4,1,'Shurikenjutsu','Maestria na utilização de shurikens e kunais no combate corpo-a-corpo ou à longa distância.','shurikenjustu.png'),
+	 (41,20,2,1,'Bloqueio Corporal','Utiliza o próprio corpo para se defender de golpes pesados.','bloqueio-corporal.png'),
+	 (41,22,1,1,'Kakuremino no Jutsu','Ninjutsu básico onde o ninja se camufla com uma capa, se confundindo com alguma parede ou pedra para pegar o inimigo de surpresa.','kakuremino-no-jutsu.png'),
+	 (41,23,0,1,'Domínio de Chakra','Adquire o controle total do chakra, podendo subir em qualquer lugar.','dominio-chakra.png'),
+	 (41,25,1,1,'Chakra Ofensivo','Concentra chakra nas mãos para bloquear algum golpe do inimigo e contra-atacar imediatamente.','chakra-defensivo.png'),
+	 (41,28,2,1,'Cabeçada','Desfere uma poderosa cabeçada no inimigo.','cabecada.png'),
+	 (41,27,3,1,'Shunshin no Jutsu','Utiliza o vento e as folhas como uma camuflagem para aparecer ou desaparecer de um lugar.','konoha-shunshin-no-jutsu.png');
+INSERT INTO item_descriptions (anime_id,item_id,item_attack_type_id,language_id,name,description,image) VALUES
+	 (41,29,4,1,'Kenjutsu','Maestria na utilização de lâminas para combate corpo-a-corpo.','kenjutsu.png'),
+	 (41,30,0,1,'Liberação de Chakra','Expande e libera chakra por todo seu corpo, fortalecendo suas energias.','liberacao-chakra.png'),
+	 (41,32,5,1,'Fuuma Shuriken','Também conhecida como Shuriken de Vento Demoníaca, é uma shuriken gigante usada em grande velocidade para cortar facilmente o inimigo.','fuuma-shuriken.png'),
+	 (41,33,3,1,'Narakumi no Jutsu','Prende o inimigo em uma ilusão causada pelo vento, fazendo-o acreditar que está imobilizado e abaixando sua defesa para um ataque fatal.','narakumi.png'),
+	 (41,34,1,1,'Kawarimi no Jutsu','Jutsu que substitui seu corpo por um tronco, enquanto você aparece em outro lugar.','kawarimi-no-jutsu.png'),
+	 (41,36,0,1,'Passo Relâmpago','Você se move em uma velocidade surpreendente, deixando um rastro de raio para trás, e acerta o inimigo no estômago, arremessando-o com a força do impulso.','PassoRelampago.png'),
+	 (41,447,0,1,'Golpe da Tartaruga Serena','Você se move calmamente, mas lendo todos os movimentos do inimigo, atacando-o bem rápido e com o acerto garantido.','tartaruga-serena.png'),
+	 (41,1327,3,1,'Drenagem Sanguínea','Usa magia para drenar aos poucos o sangue do inimigo, causando mais dano se existir algum ferimento aberto.','1327.png'),
+	 (41,1328,1,1,'Clarão Ofuscante','Aproveita a confusão cerebral do inimigo para criar um clarão e cegá-lo, criando uma abertura para atacar.','1328.png'),
+	 (41,1329,5,1,'Cristais do Zero Absoluto','Dispara diversos cristais que explodem ao tocar no inimigo, congelando seu sangue ou despedaçando-o caso já estiver congelado.','1329.png');
+INSERT INTO item_descriptions (anime_id,item_id,item_attack_type_id,language_id,name,description,image) VALUES
+	 (41,1330,2,1,'Desfiguramento Facial','Desfere uma poderosa voadora no rosto do inimigo, danificando seu crânio. Funciona melhor caso o alvo não puder se mover.','1330.png'),
+	 (41,1331,3,1,'Campo de Força','Cria uma barreira mágica capaz de te proteger de golpes podersos, possuindo uma resistência extra contra ataques comuns.','1331.png'),
+	 (41,1332,2,1,'Pulso Gravitacional','Altera a gravidade ao seu redor e enfraquece qualquer golpe inimigo, podendo parar com as mãos até mesmo os golpes mais fortes.','1332.png'),
+	 (41,1333,0,1,'Fúria Demoníaca','No seu último suspiro de vida, desperta toda a fúria guardada dentro de si, ampliando seu poder de ataque de forma desumana.','1333.png'),
+	 (41,1334,0,1,'Medo Racional','Percebe que apenas atacar pode resultar na sua morte, e racionalmente assume uma pose defensiva.','1334.png'),
+	 (41,1335,2,1,'Golpe da Nuvem Vermelha','Você se move em uma velocidade surpreendente, deixando um rastro de raio para trás, e acerta o inimigo no estômago, arremessando-o com a força do impulso.','1335.png'),
+	 (41,1336,3,1,'Esferas Radiantes','Cria diversas esferas de poder mágico que perseguem o inimigo. O impacto reduz a força dos golpes corpo-a-corpo do inimigo.','1336.png'),
+	 (41,1337,5,1,'Canhão de Pedra','Cria um gigantesco canhão feito de pedra capaz de contra-atacar um golpe mágico, reduzindo o dano recebido por você.','1337.png'),
+	 (41,1338,1,1,'Toque Eletrizante','Conduz eletricidade na palma de suas mãos, diminuindo o dano recebido por golpes de guerreiros e eletrocutando-o através da arma.','1338.png'),
+	 (41,1339,4,1,'Corte Temporal','Corta o tempo e espaço para atacar o inimigo, capaz de cortar até flechas ou balas.','1339.png');
+INSERT INTO item_descriptions (anime_id,item_id,item_attack_type_id,language_id,name,description,image) VALUES
+	 (41,1340,5,1,'Hemorragia Interna','Acerta o ferimento do inimigo à distância e abre mais o ferimento e danificando os órgãos internos, causando mais sangramento.','1340.png'),
+	 (41,1341,0,1,'Descarga Elétrica','Cria uma barreira ao seu redor que conduz eletricidade e te protege de golpes críticos.','1341.png'),
+	 (41,1342,0,1,'Alma Gêmea','Realiza ataques com a potência de duas pessoas juntas, podendo perfurar qualquer defesa.','1342.png'),
+	 (41,1343,4,1,'Desmembramento','Corta o braço ou alguma parte do inimigo de forma precisa, tornando-se quase impossível de se parar o sangramento.','1343.png'),
+	 (41,1344,1,1,'Estrondo Glacial','Cria uma poderosa tempestade que congela o ar e aos poucos o corpo do inimigo, em seguida cria um poderoso estrondo que estoura o gelo e causa um grande dano no inimigo.','1344.png'),
+	 (41,1345,2,1,'Desnorteamento','Executa um poderoso gancho no oponente capaz de danificar o cérebro e causar uma breve desorientação do inimigo.','1345.png'),
+	 (41,1346,3,1,'Tríade Dourada','Cria uma parede triangular mágica que avança diante do inimigo, tendo a chance de anular sua fraqueza contra o golpe dele.','1346.png'),
+	 (41,1714,0,1,'Benção da Deusa da Guerra','Uma energia dourada envolve seu corpo e, além de te proteger de golpes inimigos, cura seus ferimentos.','1714.png'),
+	 (41,1722,0,1,'Morte Súbita','Acaba com a vida de seu inimigo num piscar de olhos.',NULL),
+	 (41,1723,0,1,'Tentativa de Homicídio','Falha ao tentar matar o inimigo instantaneamente, porém acerta um poderoso golpe que o fere gravemente.',NULL);
+INSERT INTO item_descriptions (anime_id,item_id,item_attack_type_id,language_id,name,description,image) VALUES
+	 (41,1858,0,1,'Jutsu de Substituição','Substitue o seu próprio corpo por algum objeto, geralmente com um bloco de madeira. Isso cria uma ilusão de ótica, fazendo com que o inimigo pense que o ataque foi bem sucedido.','1858.png'),
+	 (41,2104,0,1,'Chamas da Determinação','Libera todo seu poder de ataque em troca de seu poder defensivo.','chamas-determinacao.png');
---
+++ sql/add_theme_items.sql
@@ -0,0 +1,11 @@
+INSERT INTO character_theme_items (item_id,character_theme_id,language_id,item_attack_type_id,name,description,image) VALUES
+	 (2,842,1,3,'Golpe 1',NULL,'1.png'),
+	 (5,842,1,2,'Golpe 2',NULL,'2.png'),
+	 (10,842,1,2,'Golpe 3',NULL,'3.png'),
+	 (12,842,1,3,'Golpe 4',NULL,'4.png'),
+	 (17,842,1,3,'Golpe 5',NULL,'5.png'),
+	 (19,842,1,2,'Golpe 6',NULL,'6.png'),
+	 (24,842,1,3,'Golpe 7',NULL,'7.png'),
+	 (26,842,1,3,'Golpe 8',NULL,'8.png'),
+	 (31,842,1,3,'Golpe 9',NULL,'9.png'),
+	 (35,842,1,2,'Golpe 10',NULL,'10.png');
